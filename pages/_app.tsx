69a97e255081afe485ee384fe2d2bd76bb1c73e6
Mgbemele Ikechukwu <mgbemeleikechukwu@gmail.com>
2023-03-27 13:07:30 +0100

    title update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,39 +1,43 @@
 import type {AppProps} from 'next/app';
 import '../styles/globals.css';
 
-import { useEffect, useState } from 'react';
+import Head from 'next/head';
+import {useEffect, useState} from 'react';
 
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import {GoogleOAuthProvider} from '@react-oauth/google';
 
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
 
 const MyApp = ({Component, pageProps}: AppProps) => {
-    const [isSSR, setIsSSR] = useState(true);
+  const [isSSR, setIsSSR] = useState(true);
 
-    useEffect(() => {
-        setIsSSR(false);
-    }, []);
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
 
-    if (isSSR) return null;
+  if (isSSR) return null;
 
-    return (
-        <GoogleOAuthProvider
-            clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}
-        >
-            <div className='xl:w-[1200px] m-auto overflow-hidden h-[100vh]'>
-                <Navbar />
-                <div className='flex gap-6 md:gap-20 '>
-                    <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
-                        <Sidebar />
-                    </div>
-                    <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
-                        <Component {...pageProps} />
-                    </div>
-                </div>
-             </div>
-        </GoogleOAuthProvider>
-    );
+  return (
+    <GoogleOAuthProvider
+      clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}
+    >
+      <Head>
+        <title>Tiktik</title>
+      </Head>
+      <div className='xl:w-[1200px] m-auto overflow-hidden h-[100vh]'>
+        <Navbar />
+        <div className='flex gap-6 md:gap-20 '>
+          <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
+            <Sidebar />
+          </div>
+          <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
+            <Component {...pageProps} />
+          </div>
+        </div>
+      </div>
+    </GoogleOAuthProvider>
+  );
 };
 
 export default MyApp;
