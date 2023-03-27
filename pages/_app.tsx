cb5afc12de9a3757ac79fc77d36d97c8273fa505
Mgbemele Ikechukwu <mgbemeleikechukwu@gmail.com>
2023-03-27 12:46:33 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type {AppProps} from 'next/app';
 import '../styles/globals.css';
 
 import { useEffect, useState } from 'react';
+import Head from 'next/head';
 
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
@@ -21,17 +22,21 @@ const MyApp = ({Component, pageProps}: AppProps) => {
         <GoogleOAuthProvider
             clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}
         >
-            <div className='xl:w-[1200px] m-auto overflow-hidden h-[100vh]'>
-                <Navbar />
-                <div className='flex gap-6 md:gap-20 '>
-                    <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
-                        <Sidebar />
-                    </div>
-                    <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
-                        <Component {...pageProps} />
-                    </div>
-                </div>
-            </div>
+            <Head>
+                <title>Tiktik</title>
+                    <div className='xl:w-[1200px] m-auto overflow-hidden h-[100vh]'>
+                        <Navbar />
+                        <div className='flex gap-6 md:gap-20 '>
+                            <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
+                                <Sidebar />
+                            </div>
+                            <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
+                                <Component {...pageProps} />
+                            </div>
+                        </div>
+                     </div>
+             </Head>
+            
         </GoogleOAuthProvider>
     );
 };
